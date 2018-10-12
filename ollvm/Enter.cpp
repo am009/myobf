7dcfe4536fb727c7297f38390e5e57328325c0ea
LeadroyaL <243850435@qq.com>
2018-10-12 19:34:10 +0800

    Add entry.

---
+++ ollvm/Enter.cpp
@@ -0,0 +1,23 @@
+//
+// Created by LeadroyaL on 2018/10/10.
+//
+
+#include "Transforms/Obfuscation/BogusControlFlow.h"
+#include "Transforms/Obfuscation/Flattening.h"
+#include "Transforms/Obfuscation/Split.h"
+#include "Transforms/Obfuscation/Substitution.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+using namespace llvm;
+
+static void registerOllvmPass(const PassManagerBuilder &,
+                              legacy::PassManagerBase &PM) {
+    PM.add(createBogus(true));
+    PM.add(createFlattening(true));
+    PM.add(createSplitBasicBlock(true));
+    PM.add(createSubstitution(true));
+}
+static RegisterStandardPasses
+        RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
+                       registerOllvmPass);
