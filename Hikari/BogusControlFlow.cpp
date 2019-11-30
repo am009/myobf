317135231c4cdb16346a1176f345b811d5b838f9
赵文强 <zhaowq32@chinaunicom.cn>
2019-11-30 09:47:08 +0800

    TerminatorInst has been removed.(http://lists.llvm.org/pipermail/llvm-dev/2018-May/123407.html)
    Signed-off-by: 赵文强 <zhaowq32@chinaunicom.cn>

--- Hikari/BogusControlFlow.cpp
+++ Hikari/BogusControlFlow.cpp
@@ -687,7 +687,7 @@ struct BogusControlFlow : public FunctionPass {
       for (Function::iterator fi = mi->begin(), fe = mi->end(); fi != fe;
            ++fi) {
         // fi->setName("");
-        TerminatorInst *tbb = fi->getTerminator();
+        Instruction *tbb = fi->getTerminator();
         if (tbb->getOpcode() == Instruction::Br) {
           BranchInst *br = (BranchInst *)(tbb);
           if (br->isConditional()) {
